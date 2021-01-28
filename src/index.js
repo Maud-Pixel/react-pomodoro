c95883a570007d33cb753243f4838ffabc82bd8c
Maud-Pixel <leleuxmaud@gmail.com>
2021-01-28 12:16:34 +0100

    chronomètre

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { EmailControllerApiFactory } from "mailslurp-client/dist/generated";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/app.scss";
@@ -13,12 +14,45 @@ class HelloMessage extends React.Component {
 }
 
 let App = document.getElementById("app");
-
 ReactDOM.render(<HelloMessage name="Maud" />, App);
 
-const minutes = "00";
-const seconds = "00";
-const element_minutes = <p>{minutes}</p>
-const element_secondes = <p>{seconds}</p>
-ReactDOM.render(element_minutes, document.querySelector(".number__minutes"));
-ReactDOM.render(element_secondes, document.querySelector(".number__seconds") )
+
+let minutes = 10;
+let seconds = 60;
+let count = 0;
+function decrease(){
+    if (seconds >=1)
+    {
+        var element_secondes = (
+            <p>{seconds = seconds - 1}</p>
+            );
+    }
+    else
+    {
+         element_secondes = (
+            <p>{seconds = 60 - 1}</p>
+            );  
+    }
+    count += 1;
+    if(count % 60 == 0 && minutes >0)
+    {
+        console.log("ok");
+        var element_minutes = (
+        <p>{minutes = minutes-1}</p>
+        );
+        ReactDOM.render(element_minutes, document.querySelector(".number__minutes"));
+    }
+    else if (minutes == 0 && seconds == 0)
+    {
+        minutes = "00";
+        seconds = "00";
+        ReactDOM.render(element_minutes, document.querySelector(".number__minutes"));
+        ReactDOM.render(element_secondes, document.querySelector(".number__seconds"));
+    }
+    console.log(count);
+    ReactDOM.render(element_secondes, document.querySelector(".number__seconds"));
+}
+
+setInterval(decrease, 1000);
+ 
+  
